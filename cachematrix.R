605d80f9daea11508ae44f9ee5cd0bbf15ed2683
tyagank <tyagi.ankesh@gmail.com>
2014-04-27 20:43:19 -0700

    Updated with comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,9 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## This function calculates the inverse of a matrix. It first checks for an already calcualted inverse, if there is
-## an already calculated inverse it returns that otherwise it calculates the inverse and sets inverse using setinverse() function
+## This function returns a list of 4 functions which have following description
+## set() sets the value of matrix
+## get() gets the value of matrix
+## setinverse() sets the inverse of matrix
+## getinverse() gets the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
       im <- NULL                      ##intiates inverse matrix to NULL
